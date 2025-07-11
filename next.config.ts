cd29ba7f12070e9ab44923827956ce218ae8f422
rayvego <mohit.panchal@iitgn.ac.in>
2025-07-11 14:18:36 +0530

    feat: initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextra = nextra({});
 
-export default nextConfig;
+export default withNextra({
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	},
+});
